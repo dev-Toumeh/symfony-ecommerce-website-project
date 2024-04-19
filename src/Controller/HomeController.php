e466ca01d8ed3cdaa321b292c0f019fce68b3bc6
toumeh <dev.toumeh@gmail.com>
2024-04-19 22:51:50 +0200

    create the controllers and add the frontend skeleton

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/')]
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/about')]
+    public function aboutUs(): Response
+    {
+        return $this->render('about/about.html.twig');
+    }
+
+    #[Route('/shop')]
+    public function shop(): Response
+    {
+        return $this->render('shop/shop.html.twig');
+    }
+
+    #[Route('/gallery')]
+    public function gallery(): Response
+    {
+        return $this->render('gallery/gallery.html.twig');
+    }
+
+}
---
+++ src/Controller/ShopController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ShopController extends AbstractController
+{
+    #[Route('/shop')]
+    public function shop(): Response
+    {
+        return $this->render('shop/shop.html.twig');    }
+}
